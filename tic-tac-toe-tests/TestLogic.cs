e3e8718eebca1d0398165c165a267f0a0b80ccf6
Gunnar Guðvarðarson <gunni@wave.is>
2014-11-01 15:17:53 +0000

    fixed tests and created the game state

--- tic-tac-toe-tests/TestLogic.cs
+++ tic-tac-toe-tests/TestLogic.cs
@@ -3,17 +3,23 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 using tic_tac_toe;
 
-namespace tic_tac_toe_tests
+namespace tic_tac_toe_Tests
 {
-	[TestClass]
-	class TestLogic
+	[TestClass()]
+	public class TestLogic
 	{
-		[TestMethod]
-		public void TestInstance()
+		[TestMethod()]
+		public void LogicTest()
 		{
 			Logic l = new Logic();
 
 			Assert.IsInstanceOfType(l, typeof(Logic));
+
+			// Check that gameState is all set to PlayerValue.None
+			for (int i = 0; i < l.gameState.Length; i++)
+			{
+				Assert.AreEqual(l.gameState[i], PlayerValue.None);
+			}
 		}
 	}
 }
